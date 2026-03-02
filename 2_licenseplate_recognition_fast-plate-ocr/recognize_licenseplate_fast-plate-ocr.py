27dd2b0c572686c94d5a824459fa67ec8bd8c374
Bernardo Biesseck <biesseck@gmail.com>
2026-03-01 23:01:16 -0400

    Add file '2_licenseplate_recognition_fast-plate-ocr/recognize_licenseplate_fast-plate-ocr.py'

---
+++ 2_licenseplate_recognition_fast-plate-ocr/recognize_licenseplate_fast-plate-ocr.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import cv2
+import numpy as np
+import argparse
+from pathlib import Path
+import json
+import glob
+from fast_plate_ocr import LicensePlateRecognizer
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='cct-s-v1-global-model')    # cct-xs-v1-global-model
+    parser.add_argument('--vistoria', type=str, required=True, default='C:/Users/Bernardo/GitHub/bot_download_chassi_img/qualit/vistorias_qualit/veiculos_vistoria_laudo_chassi_v2_LABELED/qualit_LABELED/vistorias_qualit_LABELED/vistorias_download_LABELED/Apto_2023-04-22_05-00-33_2023-04-28_05-42-24')
+    return parser.parse_args()
+
+
+def load_json(path: str) -> dict:
+    path = Path(path)
+    if not path.exists():
+        raise FileNotFoundError(f"JSON file not found: {path}")
+    with path.open("r", encoding="utf-8") as fh:
+        return json.load(fh)
+
+
+def resize_with_scale(image, target_size=128):
+    h, w = image.shape[:2]
+    scale = target_size / max(h, w)
+    new_w, new_h = int(w * scale), int(h * scale)
+    interpolation = cv2.INTER_AREA if scale < 1 else cv2.INTER_CUBIC
+    resized_image = cv2.resize(image, (new_w, new_h), interpolation=interpolation)
+    return resized_image, scale
+
+
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    print(f"Loading model '{args.model}'")
+    model = LicensePlateRecognizer(args.model)
+    print("    Done")
+
+    print(f"Loading vistoria '{args.vistoria}'")
+    json_path = glob.glob(os.path.join(args.vistoria, "dados_vistoria*.json").replace('\\','/'))
+    assert len(json_path) > 0, f"No JSON file found in {args.vistoria}"
+    json_path = json_path[0]
+    print(f"    Loading JSON data from: {json_path}")
+    dados_vistoria_orig = load_json(json_path)
+    dados_vistoria_corrected = {}
+    for idx_key_vistoria, key_vistoria in enumerate(dados_vistoria_orig.keys()):
+        if key_vistoria:
+            if key_vistoria.startswith("URL ") and not dados_vistoria_orig[key_vistoria] is None:
+                dados_vistoria_corrected[key_vistoria] = dados_vistoria_orig[key_vistoria].split('/')[-1]
+            else:
+                dados_vistoria_corrected[key_vistoria] = dados_vistoria_orig[key_vistoria]
+
+    if "URL Placa LABELED" in dados_vistoria_corrected and not dados_vistoria_corrected["URL Placa LABELED"] is None and dados_vistoria_corrected["URL Placa LABELED"] != "":
+        img_path = os.path.join(args.vistoria, "imgs", dados_vistoria_corrected["URL Placa LABELED"]).replace('\\','/')
+        assert os.path.isfile(img_path), f"License plate image file not found: {img_path}"
+        
+        print(f"Loading img '{img_path}'")
+        img = cv2.imread(img_path)
+        gt_placa = dados_vistoria_corrected["Placa"]
+        dados_placa_detectada = dados_vistoria_corrected["Placa LABELED DETECTED"]
+        bbox_placa = dados_placa_detectada["bbox"]
+        x1, y1, x2, y2 = int(round(bbox_placa[0])), int(round(bbox_placa[1])), int(round(bbox_placa[2])), int(round(bbox_placa[3]))
+        crop_placa = img[y1:y2, x1:x2]
+        
+        # crop_placa_resized = crop_placa
+        # crop_placa_resized = cv2.resize(crop_placa, (128, 64))
+        crop_placa_resized, scale = resize_with_scale(crop_placa, target_size=128)
+        print('crop_placa_resized.shape:', crop_placa_resized.shape)
+
+        print('Running OCR model...')
+        pred_placa = model.run(crop_placa_resized)
+        pred_placa = pred_placa[0].replace("_", "")
+        # print(f"type(pred_placa): {type(pred_placa)}")
+        print(f"    GT Placa  :", gt_placa)
+        print(f"    Pred Placa:", pred_placa)
+
+        if pred_placa == gt_placa:
+            print("    Placa reconhecida corretamente!")
+        else:
+            print("    Placa não reconhecida ou reconhecida parcialmente!")
+
+        cv2.imshow("crop_placa_resized", crop_placa_resized)
+        cv2.waitKey(0)
+
+
+    '''
+    print(f"Loading img '{args.image}'")
+    img = cv2.imread(args.image)
+    img_resized = cv2.resize(img, (640, 640))
+    cv2.imshow("img_resized", img_resized)
+    cv2.waitKey(0)
+
+    print(f"Loading model '{args.model}'")
+    model = YOLO(args.model)
+    print("    Done")
+
+    print("Performing inference...")
+    results = model.predict(source=img_resized, conf=0.60, iou=0.45, max_det=1)
+    # print("results:", results)
+
+    if len(results[0].boxes) > 0:
+        for result in results:
+            for box in result.boxes:
+                coords = box.xyxy[0].tolist()
+                conf = float(box.conf[0])
+                cls_id = int(box.cls[0])
+                label = result.names[cls_id]
+                print(f"--- Detection Found ---")
+                print(f"Label: {label}")
+                print(f"Confidence: {conf:.2%}")
+                print(f"Coordinates: x1={coords[0]:.1f}, y1={coords[1]:.1f}, x2={coords[2]:.1f}, y2={coords[3]:.1f}")
+
+                # Extract coordinates (top-left x, top-left y, bottom-right x, bottom-right y)
+                x1, y1, x2, y2 = box.xyxy[0]
+                
+                # Convert tensors/floats to integers for OpenCV
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                
+                # 4. Draw the red rectangle (BGR format: Red is 0, 0, 255)
+                cv2.rectangle(img_resized, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                
+                # Optional: Add a label
+                cv2.putText(img_resized, "License Plate", (x1, y1 - 10), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+        # 5. Show on screen
+        cv2.imshow('License Plate Detection', img_resized)
+
+        # Keep the window open until a key is pressed
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    else:
+        print("No license plate detected.")
+    '''
+        
+        
+    print("Finished\n")
